a88a1835d8f901c84d840e958a0d06303c73c11a
Adeonir Kohl <adeonir@gmail.com>
2024-09-23 19:41:41 -0300

    Update knex config and tsconfig

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,12 @@
-import { knex } from 'knex'
+import type { Knex } from 'knex'
+import knex from 'knex'
 
-export const database = knex({
+export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
     filename: './db.sqlite',
   },
   useNullAsDefault: true,
-})
+}
+
+export const database = knex(config)
