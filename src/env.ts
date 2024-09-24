e79a94e087d3528133248b1a8ace20baa831f7d3
Adeonir Kohl <adeonir@gmail.com>
2024-09-24 18:43:37 -0300

    Add database client to env

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 export const envSchema = z.object({
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
   BASE_URL: z.string().url(),
   PORT: z.coerce.number().default(3333),
