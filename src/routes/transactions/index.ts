c739b0988bd43230f5ca4fa1ffe1c57e57e1ae4c
Adeonir Kohl <adeonir@gmail.com>
2024-09-24 10:41:48 -0300

    Add session id cookie to routes

--- src/routes/transactions/index.ts
+++ src/routes/transactions/index.ts
@@ -1,4 +1,4 @@
-import crypto from 'node:crypto'
+import { randomUUID } from 'node:crypto'
 import type { FastifyInstance } from 'fastify'
 
 import { database } from '~/database.ts'
@@ -9,36 +9,70 @@ import type {
 } from './types.ts'
 import { createTransactionSchema, getTransactionSchema } from './types.ts'
 
+import { checkSessionId } from '~/middlewares/session-id.ts'
+
 export const transactions = async (app: FastifyInstance) => {
-  app.get('/', async (_, reply) => {
-    const transactions = await database('transactions').select()
+  app.get('/', { preHandler: [checkSessionId] }, async (request, reply) => {
+    const { sessionId } = request.cookies
+
+    const transactions = await database('transactions')
+      .where('sessionId', sessionId)
+      .select()
 
     return reply.send({ transactions })
   })
 
-  app.get<{ Params: GetTransactionRequest }>('/:id', async (request, reply) => {
-    const { id } = getTransactionSchema.parse(request.params)
-    const transaction = await database('transactions').where('id', id).first()
+  app.get<{ Params: GetTransactionRequest }>(
+    '/:id',
+    { preHandler: [checkSessionId] },
+    async (request, reply) => {
+      const { id } = getTransactionSchema.parse(request.params)
+      const { sessionId } = request.cookies
 
-    return reply.send({ transaction })
-  })
+      const transaction = await database('transactions')
+        .where({ id, sessionId })
+        .first()
 
-  app.get('/summary', async (_, reply) => {
-    const summary = await database('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
+      return reply.send({ transaction })
+    }
+  )
 
-    return reply.send({ summary })
-  })
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionId] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+
+      const summary = await database('transactions')
+        .where('sessionId', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
+
+      return reply.send({ summary })
+    }
+  )
 
   app.post<{ Body: CreateTransactionRequest }>('/', async (request, reply) => {
     const { title, amount, type } = createTransactionSchema.parse(request.body)
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+    }
+
     await database('transactions').insert({
-      id: crypto.randomUUID(),
+      id: randomUUID(),
+      sessionId,
       title,
       amount: amount * (type === 'income' ? 1 : -1),
       type,
+    })
+
+    reply.setCookie('sessionId', sessionId, {
+      path: '/',
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 30, // 30 days
     })
 
     return reply.status(201).send()
