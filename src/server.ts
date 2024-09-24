61ef9a9f681164c1e9ac80a388578db3c5ad4996
Adeonir Kohl <adeonir@gmail.com>
2024-09-24 10:26:52 -0300

    Install fastify/cookie

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 
 import { env } from '~/env.ts'
 import { transactions } from '~/routes/transactions/index.ts'
 
 const app = fastify()
+
+app.register(cookie)
 
 app.register(transactions, { prefix: '/transactions' })
 
